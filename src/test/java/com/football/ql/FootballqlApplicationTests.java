4b4866ffbb3774c39c83c91946e8b42a14de0c6d
Silva <larissa.silva1@dell.com>
2023-12-14 16:55:56 -0300

    :tada: First commit

---
+++ src/test/java/com/football/ql/FootballqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.ql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
